203cdb3bc67db206db461d2c039f1b2e5dd1aa56
Imad <imadhajali66@gmail.com>
2023-12-17 21:23:38 +0100

    download template

--- src/app/internes/first-resume-template/first-resume-template.component.ts
+++ src/app/internes/first-resume-template/first-resume-template.component.ts
@@ -1,6 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {IFormData, LanguageProficiency} from './first-resume-template'
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {IFormData} from './first-resume-template'
 import {UserService} from "../../services/user.service";
+import * as jsPDF from "jspdf";
+import * as jspdf from "jspdf";
+import html2canvas from "html2canvas";
+
+
 
 @Component({
   selector: 'app-first-resume-template',
@@ -8,6 +13,9 @@ import {UserService} from "../../services/user.service";
   styleUrls: ['./resume-template-style.scss','./first-resume-template.component.scss']
 })
 export class FirstResumeTemplateComponent implements OnInit{
+
+  // @ts-ignore
+  @ViewChild('appPreview', { static: false }) appPreview: ElementRef;
 
   formData: IFormData = {
     basicInfo: {
@@ -45,6 +53,30 @@ export class FirstResumeTemplateComponent implements OnInit{
   }
 
   downloadResume() {
+    const element = this.appPreview.nativeElement;
+
+    // Use html2canvas to capture the element with styles
+    html2canvas(element, { scale: 2 }).then(canvas => {
+      const imgData = canvas.toDataURL('image/png');
+
+      const pdf = new jspdf.jsPDF({
+        unit: 'mm',
+        format: 'a4',
+        orientation: 'portrait'
+      });
+
+      const imgProps = pdf.getImageProperties(imgData);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+
+      // Add the captured image to the PDF
+      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+
+      // Save the PDF or open it in a new tab as needed
+      pdf.save('resume.pdf');
+    });
   }
+
+
 
 }
